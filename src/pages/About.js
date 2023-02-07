eb259a05a0a26bfcbdc084c5d02a07dc2c5ff401
Mohit <msukhlani20@gmail.com>
2023-02-07 16:48:01 +0530

    initital commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi in
+        corrupti officiis consequuntur consequatur animi quasi vero rem harum
+        illo, cum ullam cumque dicta veritatis excepturi inventore enim aliquid
+        possimus blanditiis nemo reprehenderit ratione quam. Exercitationem
+        sapiente corrupti nulla excepturi?
+      </p>
+    </section>
+  );
+};
+
+export default About;
